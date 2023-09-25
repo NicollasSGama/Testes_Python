97bc5b454ffb97795c010229ec217d81cd47eb86
NicollasS.Gama <nicollas.programmer@gmail.com>
2023-09-25 19:02:54 -0300

    Teste de troca de telas com o PySimpleGUI

---
+++ PySimpleGUI/Testes.py
@@ -0,0 +1,44 @@
+from PySimpleGUI import (
+    Window, Button, Text, Image, Input,
+    Column, VSeparator, HSeparator, Push,
+    theme, popup, FileBrowse
+)
+
+def janela_A():
+    layout = [
+        [
+            Button('Janela A'),
+            Button('Janela B')
+        ],
+
+        [
+            Button('Botão 2')
+        ]
+    ]
+    return Window('Janela A', layout=layout)
+
+def janela_B():
+    layout = [
+        [
+            Button('Janela A'),
+            Button('Janela B')
+        ],
+    ]
+    return Window('Janela B', layout=layout)
+
+window = janela_A()
+
+while True:
+    events, values = window.read()
+    match(events):
+        case 'Janela A' | 'Janela B':
+            window.close() # Fechar a janela antes de abrir outra
+            match(events): #Trocar para outra janela pois o 'events e values' são '=' a window
+                case 'Janela A':
+                    window = janela_B()
+                case 'Janela B':
+                    window = janela_A()
+                case None:
+                    break
+
+window.close()
