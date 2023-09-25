00d3f18fd418eb01c353735ee55252e96ba36a3d
NicollasS.Gama <nicollas.programmer@gmail.com>
2023-09-25 19:03:21 -0300

    Descobrindo os popups do PySimpleGUI

---
+++ PySimpleGUI/Popup.py
@@ -0,0 +1,12 @@
+from PySimpleGUI import (
+    popup,
+    popup_ok,
+    popup_timed,
+    popup_menu,
+    popup_yes_no
+)
+
+popup('Oi, eu sou um popup')
+popup_ok('Oi, eu sou um popup')
+popup_timed('Oi, eu sou um popup')
+popup_yes_no('Oi, eu sou um popup')
