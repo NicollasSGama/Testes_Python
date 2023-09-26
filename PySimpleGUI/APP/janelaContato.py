7b807b8ef24a8a0a2c10db5cb5e874cdb142ef1b
nicollas.sgama <n@gmail.com>
2023-09-26 11:44:43 -0300

    Teste menu custom

--- PySimpleGUI/APP/janelaContato.py
+++ PySimpleGUI/APP/janelaContato.py
@@ -1,5 +1,35 @@
 from PySimpleGUI import (
     Window, Button, Text, Image, Input,
     Column, VSeparator, HSeparator, Push,
-    theme, popup, Checkbox
+    theme, popup, Checkbox, MenubarCustom,
+    Menubar
 )
+def janela_contato():
+    theme('DarkPurple')
+
+    layout = [
+        [
+            Menubar(
+                [
+                    ['Arquivo', ['Sair']],
+
+                    ['Editar',['Edite-me']]
+                ]
+            )
+        ],
+
+        [
+            MenubarCustom(
+                [
+                    ['Nada', ['Nada']]
+                ]
+            )
+        ]
+    ]
+    return Window('Menu',
+                  layout=layout,
+                  size=(600, 400)
+                  )
+
+contato = janela_contato()
+contato.read()
