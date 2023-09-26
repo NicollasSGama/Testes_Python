061eaa874a4e64c6fd16bc30dd4185e32c3f1b53
nicollas.sgama <n@gmail.com>
2023-09-26 10:55:33 -0300

    Construcao da janela para recuperar senha

---
+++ PySimpleGUI/APP/janelaRecuperarSenha.py
@@ -0,0 +1,101 @@
+from PySimpleGUI import (
+    Window, Button, Text, Image, Input,
+    Column, VSeparator, HSeparator, Push,
+    theme, popup, Checkbox
+)
+
+def janelaRecuperarSenha():
+    theme('Dark Purple')
+    layout = [
+        # ------------------------------------------------------------------
+        #   Parte superior e botão voltar
+        # ------------------------------------------------------------------
+        [
+            Image(),
+
+            Push(),
+
+            Button('VOLTAR')
+        ],
+        # ------------------------------------------------------------------
+        #   Linha horizontal
+        # ------------------------------------------------------------------
+        [
+            HSeparator()
+        ],
+        # ------------------------------------------------------------------
+        #   Parte: Recuperar senha
+        # ------------------------------------------------------------------
+        [
+            Text('RECUPERAR SENHA')
+        ],
+
+        [
+            Text('CPF')
+        ],
+
+        [
+            Input('ex.: 123.456.789-x')
+        ],
+        # ------------------------------------------------------------------
+        #   Parte inferior e botão recuperar
+        # ------------------------------------------------------------------
+        [
+            Push(),
+
+            Button('Recuperar')
+        ]
+    ]
+    return Window('Recuperar',
+                  layout=layout,
+                  #size=(300, 320)
+                  )
+janelacpf = janelaRecuperarSenha()
+janelacpf.read()
+
+def janelaNovaSenha():
+    theme('Dark Purple')
+
+    layout = [
+        # ------------------------------------------------------------------
+        #   Parte superior e botão voltar
+        # ------------------------------------------------------------------
+        [
+            Image(),
+
+            Push(),
+
+            Button('Voltar')
+        ],
+        # ------------------------------------------------------------------
+        #   Recuperar senha (nova)
+        # ------------------------------------------------------------------
+        [
+            Text('Nova Senha'),
+        ],
+
+        [
+            Input('*****'),
+        ],
+
+        [
+            Text('Nova Senha(repetir)')
+        ],
+
+        [
+            Input('*****')
+        ],
+        # ------------------------------------------------------------------
+        #   Parte inferior e botão recuperar
+        # ------------------------------------------------------------------
+        [
+            Push(),
+
+            Button('RECUPERAR')
+        ]
+    ]
+    return Window('NovaSenha',
+                  layout=layout
+                  )
+janelasenha = janelaNovaSenha()
+janelasenha.read()
