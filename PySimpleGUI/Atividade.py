4ec5d794c575b49785b079c4cfa6e1150ee57410
nicollas.sgama <n@gmail.com>
2023-09-28 11:52:04 -0300

    Comeco

--- PySimpleGUI/Atividade.py
+++ PySimpleGUI/Atividade.py
@@ -1,3 +1,36 @@
 # Criar uma tela
 # Atribuir dados
 # Salvar no txt
+
+from PySimpleGUI import (
+    Text, Input, Button, Column,
+    HSeparator, VSeparator, Push,
+    Window, Frame
+)
+
+def janela_atividade():
+
+    layout = [
+        [
+            Text('Nome')
+        ],
+
+        [
+            Input()
+        ],
+
+        [
+
+        ]
+    ]
+
+    layout = [
+        [
+            Frame('Dados',
+                  layout)
+        ]
+    ]
+    return Window('ATIVIDADE',
+                  layout)
+
+janela_atividade().read()
