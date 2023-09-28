189a708fdaadc4856f51d1b8cc017aef8530704e
NicollasS.Gama <nicollas.programmer@gmail.com>
2023-09-28 15:36:51 -0300

    Atividade

--- PySimpleGUI/Atividade.py
+++ PySimpleGUI/Atividade.py
@@ -5,29 +5,110 @@
 from PySimpleGUI import (
     Text, Input, Button, Column,
     HSeparator, VSeparator, Push,
-    Window, Frame
+    Window, Frame, CalendarButton,
+    theme
 )
 
 def janela_atividade():
+    theme('DarkBlue3')
 
-    layout = [
+    layout_esquerdo = [
         [
             Text('Nome')
         ],
 
         [
-            Input()
+            Input(key='-NOME-')
         ],
 
         [
+            Text('Sobrenome')
+        ],
 
+        [
+            Input(key='-SOBRENOME-')
+        ],
+
+        [
+            Text('CPF')
+        ],
+
+        [
+            Input(key='-CPF-')
+        ],
+
+        [
+            HSeparator()
+        ],
+
+        [
+            Button('CHECAR',
+                   key='CPF_B'),
+        ]
+    ]
+
+    layout_direito = [
+        [
+            Text('Senha')
+        ],
+
+        [
+            Input(key='-SENHA-')
+        ],
+
+        [
+            Text('Senha (Confirmar)')
+        ],
+
+        [
+            Input(key='-SENHA_C-')
+        ],
+
+
+        [
+            Text('Nascimento')
+        ],
+
+        [
+            Input(readonly=True,
+                  enable_events=True,
+                  key='-CALENDARIO-')
+        ],
+
+        [
+            HSeparator()
+        ],
+
+        [
+            CalendarButton('CALENDÁRIO',
+                           default_date_m_d_y=(1, 1, 2023),
+                           format='%d-%m-%y',
+                           close_when_date_chosen=False,
+                           key='-CALENDARIO_B-'
+                           ),
+
+            Push(),
+
+            Button('REGISTRAR',
+                   key='-REGISTRAR-',
+                   )
+        ]
+    ]
+
+    layout_total = [
+        [
+            Column(layout_esquerdo),
+
+            VSeparator(),
+
+            Column(layout_direito)
         ]
     ]
 
     layout = [
         [
-            Frame('Dados',
-                  layout)
+            Frame('DADOS',
+                  layout_total)
         ]
     ]
     return Window('ATIVIDADE',
